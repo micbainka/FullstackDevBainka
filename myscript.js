cc4edc4dffd3ad8330122ddd228e1e4173c66d15
Michal <michal.bainka@bayer.com>
2024-07-13 15:00:32 +0200

    Exercise1

--- myscript.js
+++ myscript.js
@@ -1,29 +1,5 @@
-//alert("Hello world")
+let zmienna = 12
+console.log(typeof zmienna)
 
-//Numeric
-let length = 180 
-//console.log(length)
-//alert (length) 
-let weigth = 85.5
-//alert(weigth)
-
-
-//Strings
-let color = "Yellow"
-console.log(color)
-
-//Boolean
-
-let x = true
-console.log(x)
-let y = false
-console.log(y)
-console.log (typeof y)
-
-//Object
-const person  = {firstName: "Marek", lastName:"Wisniewksi" }
-console.log(person)
-
-//Array
-const cars = ["Saab","Volvo","BMW"]
-console.log(cars)
+zmienna = "Język Java script"
+console.log(typeof zmienna)
