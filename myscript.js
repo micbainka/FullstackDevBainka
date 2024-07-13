4f95c43ff0ddc55171cf4278315654d58303ba41
Michal <michal.bainka@bayer.com>
2024-07-13 11:30:48 +0200

    data types

---
+++ myscript.js
@@ -0,0 +1,33 @@
+//alert("Hello world")
+
+//Numeric
+let length = 180 
+//console.log(length)
+//alert (length) 
+let weigth = 85.5
+//alert(weigth)
+
+
+//Strings
+let color = "Yellow"
+console.log(color)
+
+//Boolean
+
+let x = true
+console.log(x)
+let y = false
+console.log(y)
+
+
+//Object
+const person  = {firstName: "Marek", lastName:"Wisniewksi" }
+console.log(person)
+
+//Array
+const cars = ["Saab","Volvo","BMW"]
+console.log(cars)
+
+//Date
+const date = new Date("2024-08-31")
+console.log(date)
