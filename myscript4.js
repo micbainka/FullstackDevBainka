636c7a929f3cd3ecacf900d161aaf8b75262001e
Michal <michal.bainka@bayer.com>
2024-07-13 14:16:47 +0200

    functions

---
+++ myscript4.js
@@ -0,0 +1,30 @@
+function basicAdd(a,b){
+    return a+b
+}
+const newAdd = (a,b) => a + b 
+
+const addAndPrint = (a,b) => {
+    let ret = a + b
+    console.log("add and print ", ret)
+    return ret
+}
+
+window.onload = function (){
+    console.log(basicAdd(2, 2))
+   // console.log("New add:" newAdd( 2, 2))
+    addAndPrint (2,2)
+
+    let array = [1,2,3,4]
+    
+    array.forEach(element => {
+        console.log(element)
+        
+    })
+}
+
+window.onload = function(){
+    document.getElementById("action button").onclick = function(){
+        alert(document.getElementById("inputbox").value)
+    }
+}
+
