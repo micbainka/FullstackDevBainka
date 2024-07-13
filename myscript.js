3ca7db4a3b91c04cfdc1e9d6432f7f4e0d1e770b
Michal <michal.bainka@bayer.com>
2024-07-13 14:17:33 +0200

    arrow functions

--- myscript.js
+++ myscript.js
@@ -1,106 +1,34 @@
-function examplefunction(){}
-
-//alert("Hello world")
-
-//Numeric
-let length = 180 
-//console.log(length)
-//alert (length) 
-let weigth = 85.5
-//alert(weigth)
-
-
-//Strings
-let color = "Yellow"
-console.log(color)
-
-//Boolean
-
-let x = true
-console.log(x)
-let y = false
-console.log(y)
-
-
-//Object
-const person  = {firstName: "Marek", lastName:"Wisniewski" }
-console.log(person)
-
-//Array
-const cars = ["Saab","Volvo","BMW"]
-console.log(cars)
-
-//Date
-const date = new Date("2024-08-31")
-console.log(date)
-
-//Equal
-console.log ("Wynik porownania: "+ (length==weigth))
-
-//Equal and type
-console.log ("Wynik porownania: "+ (length===weigth))
-
-//Not Equal
-console.log(100 != 101)
-
-//Not Equal and type
-console.log(100 !== 101)
-
-//Greater 
-console.log(100 > 101)
-
-//Greater or eq
-console.log(100 >= 101)
-
-//Ternary operator
-let result = 100 > 101 ? "TAK" : "NIE"
-console.log(result)
-
-//loops 
-
-//for loop
-
-for(let i = 0; i < 9; i =i+1) // i+1 tak samo i++
-    {
-        console.log(i)
+function examplefunction(){
+    console.log("Function")
+}
+    window.onload = function(){
+        examplefunction()
     }
 
-    for(let i = 9; i >= 0; i =i-1) // i+1 tak samo i++
-    {
-        console.log(i)
+    function substractArgs(arg1, arg2){
+        console.log(arg1 - arg2)
     }
 
-    //foreach
+function calculateAge(yearOfBirth){
+    return 2024 - yearOfBirth
+}
+function checkMovie(age,money){
+    if(age>= 18 && money >= 30){
+        return true
+    }else{
+        return false
+    }
+}
+window.onload = function(){
+    examplefunction()
+    substractArgs(2,2)
+    substractArgs(2,3)
+    substractArgs(5,10)
     
-    cars.forEach(function(element)
-    {console.log(element)})
+let myAge = calculateAge(1992)
+console.log("my age is "+ myAge)
 
-
-    //If ELSE
-
-//IF ELSE
-let themperature = 38.5
-if(themperature >= 36 && themperature < 37){
-    console.log("W normie")
-}else if(themperature >= 37 && themperature < 38){
-    console.log("Stan podgorączkowy")
-}else if(themperature >= 38 && themperature < 40){
-    console.log("Gorączka")
-}else{
-    console.log("Błąd pomiaru")
+console.log(checkMovie(myAge, money))
 }
 
-
-let money = 15
-let voucher = false
-
-if(money > 20 || voucher){
-    console.log("miłego seansu")
-}else {
-            console.log("zapraszam do parku")
-        }
-
-//NOT 
-if(!(15>20)){
-    console.log("NOT")
-}
+function basicAdd(a,b)
