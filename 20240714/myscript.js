10bf3b531185dbfa82bdc2e5f0f92409ca9e3140
Michal <michal.bainka@bayer.com>
2024-07-21 15:46:33 +0200

    Logging success and errors

--- 20240714/myscript.js
+++ 20240714/myscript.js
@@ -1,25 +1,51 @@
-//STANDARD
-
-function doThings(){
-    return Promise.resolve("ok")
+function loadUserData(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("User Data")
+        }, 1000)
+        
+    }
+)
+}
+function loadBooks()
+{
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject("Error while downloading Books") 
+        }, 1000)
+    })
 }
 
-doThings()
-.then(res => {
-    console.log(res)
-})
+function loadProducts()
+{
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Products Data ")
+    }, 1000)
 
-//ASYNC
+    })
+} 
 
-async function doThingsAsync(){
-    //return "ok"
-    return Promise.reject ("not ok")
+async function render(){
+    const user = await loadUserData()
+
+
+    let books 
+    loadBooks()
+    .then(result => {
+        books = result
+    })
+    .catch(error => {
+        console.log(error)
+        books = ""
+    }
+
+    )
+    //const books = await loadBooks() //await gwarantuje ze czekamy na dane bez awaita funkcja laduje tylko status pending
+    const Products = await loadProducts()
+    console.log(user, books, Products)
 }
 
-doThingsAsync()
-.then (res => {
-    console.log(res)
-})
-.catch(error => {
-    console.log(error)
-})
+window.onload = function() {
+    render()
+}
