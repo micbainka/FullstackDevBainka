638d7cf231f0b2ecfec5ba0b9cd1e085ef7cf1cd
Michal <michal.bainka@bayer.com>
2024-07-21 16:56:24 +0200

    factory

--- 20240714/myscript.js
+++ 20240714/myscript.js
@@ -1,9 +1,25 @@
-class Product {
-    display (){
-        console.log("New Product Created")
+class classProduct {
+
+    constructor(width,heigth,weigth){
+        this.width = width
+        this.heigth = heigth
+        this.weigth = weigth
+    }
+
+display(){
+    console.log("Product specification (width:" + this.width + " height: " + this.heigth + " weight: "+ this.weigth + ")" )
     }
 }
 
+class classProductFactory{
+    createProduct(width,heigth,weigth){
+        return new classProduct(width,heigth,weigth)
+    }
+}
 
-const product = new Product()
+const factory = new classProductFactory()
+
+const product = factory.createProduct(10,20,30);
+const product2 = factory.createProduct(20,30,40);
 product.display()
+product2.display()
