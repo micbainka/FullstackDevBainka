ac9df2e663f8047020ef7d0d4401e2d4b5ad627a
Michal <michal.bainka@bayer.com>
2024-07-21 16:07:53 +0200

    class Config

--- 20240714/myscript.js
+++ 20240714/myscript.js
@@ -1,51 +1,27 @@
-function loadUserData(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("User Data")
-        }, 1000)
-        
+/*class AppConfig{
+    firstName = "John"
+    lastName = "Doe"
+    age = 15
+}*/
+/*
+const myInstance = new AppConfig()
+console.log(myInstance)*/
+
+class AppConfig{
+    constructor(number = 5){
+    if(AppConfig.exists){
+        return AppConfig.instance 
     }
-)
-}
-function loadBooks()
-{
-    return new Promise((resolve,reject) => {
-        setTimeout(() => {
-            reject("Error while downloading Books") 
-        }, 1000)
-    })
-}
-
-function loadProducts()
-{
-return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Products Data ")
-    }, 1000)
-
-    })
-} 
-
-async function render(){
-    const user = await loadUserData()
-
-
-    let books 
-    loadBooks()
-    .then(result => {
-        books = result
-    })
-    .catch(error => {
-        console.log(error)
-        books = ""
+    this.randomNumber = Math.random()
+    this.number = number
+    AppConfig.exists = true
+    AppConfig.instance = this
+    return this
     }
-
-    )
-    //const books = await loadBooks() //await gwarantuje ze czekamy na dane bez awaita funkcja laduje tylko status pending
-    const Products = await loadProducts()
-    console.log(user, books, Products)
 }
 
-window.onload = function() {
-    render()
-}
+const configObject = new AppConfig(8)
+const configObject2 = new AppConfig(1)
+
+console.log(configObject)
+console.log(configObject2)
