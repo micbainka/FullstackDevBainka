3f0f446953d50773633a60b5efd724ed9c090a49
Michal <michal.bainka@bayer.com>
2024-07-21 16:14:46 +0200

    singleton

--- 20240714/myscript.js
+++ 20240714/myscript.js
@@ -8,20 +8,22 @@ const myInstance = new AppConfig()
 console.log(myInstance)*/
 
 class AppConfig{
-    constructor(number = 5){
+    constructor(number = 5, darkMotive = false){
     if(AppConfig.exists){
         return AppConfig.instance 
     }
     this.randomNumber = Math.random()
     this.number = number
+    this.darkMotive = darkMotive
     AppConfig.exists = true
     AppConfig.instance = this
     return this
     }
 }
 
-const configObject = new AppConfig(8)
+const configObject = new AppConfig(2)
 const configObject2 = new AppConfig(1)
 
 console.log(configObject)
 console.log(configObject2)
+console.log(configObject===configObject2)
