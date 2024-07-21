3538de5f0826645fc8f977b07069b61739b8b1f9
Michal <michal.bainka@bayer.com>
2024-07-21 11:54:25 +0200

    calculator excercise and callBack

---
+++ 20240714/myscript.js
@@ -0,0 +1,14 @@
+function myDisplayer(text){
+    alert(text)
+}
+function myCalculator(num1, num2, myCallback){
+    let sum = num1 + num2
+    myCallback(sum)
+}
+
+myCalculator(5,5, myDisplayer)
+
+window.onload = function(){
+    myCalculator(5,5, myDisplayer)
+}
+
--- myscript.js
+++
@@ -1,5 +0,0 @@
-let zmienna = 12
-console.log(typeof zmienna)
-
-zmienna = "Język Java script"
-console.log(typeof zmienna)
